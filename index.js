ea029379c6e38c3e0a99f74fd2ef395b41a0b9f0
mitchshumway <76656075+mitchshumway@users.noreply.github.com>
2021-09-29 06:36:35 -0400

    port fix?

--- index.js
+++ index.js
@@ -101,3 +101,9 @@ app.get('/account/all', function (req, res) {
 // var port = 3000;
 // app.listen(port);
 // console.log('Running on port: ' + port);
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
